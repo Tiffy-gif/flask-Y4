de68994e84eacbcda9b96a371a80e931a1a9f92b
hellomehf <klok8670@gmail.com>
2026-01-02 10:14:31 +0700

    Update user.py

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ class User(db.Model):
     branch_id = db.Column(db.Integer, index=True)
     username = db.Column(db.String(128))
     email = db.Column(db.String(128), nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     phone = db.Column(db.String(128), nullable=True)
     role = db.Column(db.String(128), nullable=True)
     image = db.Column(db.String(128), nullable=True)
